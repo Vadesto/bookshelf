a44d447b78b58ef7f053404cdc183d0166bd5d25
Vladislav Karpushkin <vadeesto@gmail.com>
2020-03-11 00:59:46 +0700

    model for authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_184303) do
+ActiveRecord::Schema.define(version: 2020_03_10_174444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "isbn", null: false
