e8c1a7a40b36b56ce3005ec1022f63753d3f91cd
Vladislav Karpushkin <vadeesto@gmail.com>
2020-04-19 19:17:12 +0700

    Author's pages

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,6 +3,7 @@
 class Author < ApplicationRecord
   has_many :author_books, dependent: :restrict_with_error
   has_many :books, through: :author_books
+  has_many :five_books, -> { order(created_at: :desc).limit(5) }, through: :author_books, source: :book
 
   validates :name,
     presence: true,
