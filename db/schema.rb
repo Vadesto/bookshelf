06b86b0f60212820d655c4804099329d1b2ec7eb
Vladislav Karpushkin <vadeesto@gmail.com>
2020-04-26 22:59:20 +0700

    new column in book_rent_history_item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_192907) do
+ActiveRecord::Schema.define(version: 2020_04_26_143953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_03_28_192907) do
     t.bigint "book_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0, null: false
     t.index ["book_id"], name: "index_book_rent_history_items_on_book_id"
   end
 
