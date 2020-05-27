dcf21e20fd658e5ae6f898d977cc51023b70ebca
Vladislav Karpushkin <vadeesto@gmail.com>
2020-05-27 23:32:50 +0700

    New blocks at the main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,7 @@ class HomeController < ApplicationController
   def index
     @last_books = Book.order(created_at: :desc).limit(11)
     @last_authors = Author.order(created_at: :desc).limit(5)
+    @delayed_books = Book.where(status: :delayed).order(created_at: :desc).limit(6)
+    @reading_books = Book.where(status: :reading).order(created_at: :desc).limit(6)
   end
 end
