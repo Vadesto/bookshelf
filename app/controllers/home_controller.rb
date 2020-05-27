0ac7708f92113c904796649fa9e03bd59a75a54d
Vladislav Karpushkin <vadeesto@gmail.com>
2020-05-27 23:21:35 +0700

    Auhors panel on the main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
   # GET /
   def index
     @last_books = Book.order(created_at: :desc).limit(11)
+    @last_authors = Author.order(created_at: :desc).limit(5)
   end
 end
